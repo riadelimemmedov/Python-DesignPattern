a6c1cd29fc23e5a97060b116af390f1688099a47
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-04-07 22:41:10 +0400

    Add FlyWeight Pattern

---
+++ structural_pattern/flyweight.py
@@ -0,0 +1,122 @@
+from abc import ABC,abstractmethod
+import random
+
+
+#*Method1
+# #!Sprite
+# class Sprite(ABC):
+#     @abstractmethod
+#     def draw(self):
+#         pass
+    
+#     @abstractmethod
+#     def move(self,x:int,y:int):
+#         pass
+    
+
+
+# #?FigtherRank
+# class FigtherRank:
+#     private = 0
+#     sergeant = 1
+#     major = 2
+
+
+# #!Figther
+# class Figther(Sprite):
+#     def __init__(self,rank:FigtherRank):
+#         self.rank = rank
+        
+#     def draw(self):
+#         print(f"Drawing Figther {self}")
+        
+#     def move(self,x:int,y:int):
+#         print(f"Moving Figther {self} to position {x}, {y}")
+        
+
+# #!FigtherFactory
+# class FigtherFactory:
+#     def __init__(self):
+#         self.figthers = []
+        
+    
+#     def get_figther(self,rank:FigtherRank):
+#         f = self.figthers[rank]
+#         if not f:
+#             f = Figther(rank)
+#             self.figthers[rank] = f
+#             print('Rank ', rank)
+#             print('Figther ', f)
+#         return f
+    
+
+# #!Army
+# class Army:
+#     army = []
+    
+#     def spam_figther(self,rank:FigtherRank):
+#         self.army.append(Figther(rank))
+        
+#     def draw_army(self):
+#         for figther in self.army:
+#             if figther.rank == FigtherRank.major:
+#                 print('M',end='')
+#             elif figther.rank == FigtherRank.sergeant:
+#                 print('S',end='')
+#             else:
+#                 print('P',end='')
+
+# if __name__ == '__main__':
+#     army_size = 1000
+#     army = Army()
+    
+#     for i in range(army_size):
+#         r = random.randrange(3)
+#         army.spam_figther(r)
+        
+#     army.draw_army()
+
+
+#*Method2
+#!Flyweight
+class Flyweight:
+    _shared_state = {}
+    
+    def __init__(self,intrinsic_state):
+        print('Dict value is before ', self.__dict__)
+        self.__dict__ = self._shared_state
+        self.intrinsic_state = intrinsic_state
+        print('Dict value is after ', self.__dict__)
+        
+    def operation(self,extrinsic_state):
+        print(f"Intrinsic state : {self.intrinsic_state}\n. Extrinsic state : {extrinsic_state}")
+
+#!FlyweightFactory
+class FlyweightFactory:
+    _flyweights = {}
+    
+    @classmethod
+    def get_flyweight(cls,intrinsic_state):
+        if intrinsic_state not in cls._flyweights:
+            print('Intrinsic state value is ', intrinsic_state)
+            cls._flyweights[intrinsic_state] = Flyweight(intrinsic_state)
+        #else
+        return cls._flyweights[intrinsic_state]
+    
+    
+if __name__ =='__main__':
+    factory = FlyweightFactory()
+    
+    flyweight1 = factory.get_flyweight('state1')
+    flyweight1.operation('A')
+    print('Flyweight1 ', flyweight1)
+    
+    
+    flyweight2 = factory.get_flyweight('state2')
+    flyweight2.operation('B')
+    print('Flyweight2 ', flyweight2)
+    
+    
+    flyweight3 = factory.get_flyweight('state1')
+    flyweight3.operation('C')
+    print('Flyweight3 ', flyweight1)
